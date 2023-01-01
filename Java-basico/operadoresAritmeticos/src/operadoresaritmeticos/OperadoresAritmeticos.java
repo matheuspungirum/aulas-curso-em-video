7f50ce91e2ff185ca82fa090e3ac995d8c811e70
matheuspungirum <matheus_pungirum@hotmail.com>
2023-01-01 19:58:31 -0300

    Inclusão de Operadores Aritméticos

---
+++ Java-basico/operadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author mathe
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       /* int n1 = 3;
+        int n2 = 5;
+        float n = (n1+n2)/2;  
+        System.out.println ("A média é igual a " + n);*/
+        
+        /*int numero = 10;
+        int valor = 4 + numero--;
+        System.out.println (valor);
+        System.out.println (numero);*/
+        
+       /* int x = 4;
+        x += 2; //x = x + 2
+        System.out.println(x);*/
+       
+       /*float v = 8.9f;
+       int ar = (int) Math.floor(v);
+       System.out.println(ar);*/
+       
+      /* float v = 8.9f;
+       int ar = (int) Math.ceil(v);
+       System.out.println(ar);*/
+      
+      /*float v = 8.6f;
+       int ar = (int) Math.round(v);
+       System.out.println(ar);*/
+      
+      double ale = Math.random();
+      int n = (int) (15 + ale * (50-15));
+        System.out.println(n);
+       
+       
+    }
+    
+}
